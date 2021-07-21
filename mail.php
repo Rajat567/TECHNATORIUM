3078f88dfa54dc77f4e69ac00e237d0a356e1f12
Rajat567 <rajattripathi243@gmail.com>
2021-07-21 13:46:03 +0530

    MADE WEBMAIL

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$to = "rajattripathi243@email.com";
+$subject = "Mail From My Portfolio";
+$txt = "First Name ". $firstname . "\r\n Last Name = " . $lastname . "\r\n Email = " . $email . "\r\n Phone = " . $phone . "\r\n Message = " . $message;
+$headers = "From: noreply@rajat.soumojit.in" . "\r\n" ;
+if($email=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
