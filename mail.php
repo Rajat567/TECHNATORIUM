eec08a3c98a500594fe1b351b78257013a6d2532
Rajat567 <rajattripathi243@gmail.com>
2021-07-21 13:57:47 +0530

    added location

--- mail.php
+++ mail.php
@@ -4,11 +4,12 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$to = "rajattripathi243@email.com";
+$to = "rajattripathi243@gmail.com";
 $subject = "Mail From My Portfolio";
 $txt = "First Name ". $firstname . "\r\n Last Name = " . $lastname . "\r\n Email = " . $email . "\r\n Phone = " . $phone . "\r\n Message = " . $message;
-$headers = "From: noreply@technatorium.com" . "\r\n" ;
-if($email=NULL){
+$headers = "From: noreply@rajat.soumojit.in" . "\r\n" ;
+if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    header("Location:thankyou.html");
 }
 ?>
